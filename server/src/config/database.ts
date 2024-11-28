4f827e65ca1648585e311e3426054b232a485b86
asna sharin <asnasherin1233@gmail.com>
2024-11-28 23:43:40 +0530

    initial commit

---
+++ server/src/config/database.ts
@@ -0,0 +1,26 @@
+import app from "../app";
+import mongoose from "mongoose";
+import { job } from "../utils/expiryScript";
+import { SpotJob } from "../utils/renewSpot";
+
+const PORT = process.env.PORT;
+
+async function startServer() {
+
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
+    console.log("Database Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server Running on Port ${PORT}`);
+      job;
+      SpotJob;
+    });
+    
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+
+}
+
+startServer();

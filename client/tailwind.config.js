949f26a4f0b32d6f7289a47a632ce97a5afe855a
asna sharin <asnasherin1233@gmail.com>
2025-02-05 13:03:48 +0530

    fix errors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,32 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./node_modules/flowbite-react/lib/**/*.{js,ts}",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         'color-one': '#2b6777',
+//         'color-two': '#c8d8e4',
+//         'color-three': '#ffffff',
+//         'color-four': '#f2f2f2',
+//         'color-five': '#0d9488',
+//         'background-one': "#020914",
+//         'background-two': "#0d1117",
+//         'home-color': "#1496BB",
+//       },
+//     },
+//   },
+//   plugins: [import('flowbite/plugin')],
+// }
+
+
+
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin.js"; 
+
 export default {
   content: [
     "./index.html",
@@ -19,6 +47,5 @@ export default {
       },
     },
   },
-  plugins: [import('flowbite/plugin')],
-}
-
+  plugins: [flowbitePlugin], 
+};
